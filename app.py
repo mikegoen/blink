f2b01678cd5ad1ad9eace86fb0fa0f728a821400
Mike Goen <michael.goen89@gmail.com>
2022-01-13 17:22:09 -0800

    check status of sleep job

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
-
 from redis import Redis
 from rq import Queue
-from datetime import timedelta
+from rq.registry import ScheduledJobRegistry
+from rq.job import Job
+from datetime import datetime, timedelta
+
+
 from blink import BlinkAccount, arm_module, disarm_module
 
 app = Flask(__name__)
@@ -21,21 +24,41 @@ def fetch():
 
 @app.route("/status")
 def status():
+    #check if sleeping first
+    registry = ScheduledJobRegistry(queue=q)
+    scheduledIDs = registry.get_job_ids()
+    if len(scheduledIDs) > 1:
+        return "more than one job scheduled, something isn't right"
+    elif len(scheduledIDs) == 1:
+        time_sleeping_until = registry.get_scheduled_time(scheduledIDs[0])
+    else:
+        time_sleeping_until = False
+
     BlinkAccount.refresh()
     sync = BlinkAccount.sync["Home1"]
-    status = f"{sync.name}: {'Armed' if sync.arm else 'Disarmed'}"
-    return status
+
+    if time_sleeping_until and sync.arm == False:
+        return "Module sleeping"
+    elif sync.arm:
+        return "Armed"
+    else:
+        return "Disarmed"
+
 
 @app.route("/sleep", methods=["POST"])
 def sleep():
-    # TODO: check if it's already disarmed, and send response
     data = request.get_json()
 
-    if 'sleepminutes' in data:
+    registry = ScheduledJobRegistry(queue=q)
+    scheduledIDs = registry.get_job_ids()
+
+    if 'sleepminutes' in data and len(scheduledIDs) == 0:
         minutes = int(data['sleepminutes'])
         disarm_module()
         job = q.enqueue_in(timedelta(minutes=minutes), arm_module)
-        return "OK"
+        return "Going to sleep"
+    elif len(scheduledIDs) > 0:
+        return "already sleeping"
     else:
         return "bad request"
         
